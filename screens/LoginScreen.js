3bc84b0492d7d2c165396950100c4681f0ff541a
Nicolas Corbalan <outreef@hotmail.com>
2021-01-26 00:02:25 +0100

    styling login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { KeyboardAvoidingView } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import {Button, Input, Image} from 'react-native-elements';
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View>
+    <KeyboardAvoidingView behaviour='padding' enabled style={styles.container}>
       <Image 
         source={{
         uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png"
       }}
         style={{width: 200, height: 200}}
       />
-      <Text>I am the Login screen</Text>
-    </View>
+      <View>
+        <Input placeholder='Email' autoFocus type='email'
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        />
+        <Input placeholder='Password' 
+        secureTextEntry type='password'
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button  containerStyle={styles.button} title='Login'/>
+      <Button 
+      containerStyle={styles.button}
+      title='Register' type='outline'/>
+    </KeyboardAvoidingView>
   )
 }
 
 export default LoginScreen
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  button: {
+    width: 200,
+    marginTop: 10
+  }
 })
